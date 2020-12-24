1498bcbfa57c2b87f6d2ff1d183cd4e9e5970964
Ben Doyle <bdoyle@protonmail.ch>
2020-12-24 13:06:17 -0500

    Add nix wrapper for eslint

---
+++ nix/eslint.nix
@@ -0,0 +1,14 @@
+{ lintCmd ? ""}:
+
+let runScript = ''
+      #! /usr/bin/env bash
+      if [ ! -d node_modules ];
+      then
+         npm install
+      fi;
+         node_modules/eslint/bin/eslint.js ${lintCmd}
+    '';
+
+    lint = import ./env.nix { inherit runScript; };
+
+in lint
