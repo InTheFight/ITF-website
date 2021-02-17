9f7d90a384e9bc119680fe2657233fc0a9eb0899
Ben Doyle <bdoyle@protonmail.ch>
2021-02-17 17:28:09 -0500

    eslint

--- src/components/molecules/placeholderLinks.js
+++ src/components/molecules/placeholderLinks.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-
 const PurpleButton = styled.button`
   background: #2C358F;
   border: none;
@@ -16,35 +15,36 @@ const PurpleButton = styled.button`
   width: 280px;
 `;
 
-const Button = ({ color, text }) => {
-    return (
-      <PurpleButton>{text}</PurpleButton>
-    );
-};
+const Button = ({ text }) => (
+  <PurpleButton>{text}</PurpleButton>
+);
 
-Button.defaultProps = {
-  color: '',
-  type: '',
-  onClick: () => {},
-};
 const Container = styled.div`
   align-items: center;
   justify-content: center;
   display: flex;
   flex-direction: column;
   margin-bottom: 24px;
-`
+`;
 export const PlaceholderLinks = styled.ul`
   list-style: none;
-`
+`;
 
-export const PlaceholderLink = ({text, href}) => {
+export const PlaceholderLink = ({ text, href }) => (
+  <Container>
+    <li>
+      <a href={href}>
+        <Button type="button" color="purple" text={text} />
+      </a>
+    </li>
+  </Container>
+);
 
-    return <Container>
-             <li>
-               <a href={href}>
-                 <Button type="button" color="purple" text={text}/>
-               </a>
-             </li>
-           </Container>
-}
+PlaceholderLink.propTypes = {
+  text: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+};
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,9 @@ import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import Layout from '../components/templates/layout';
 import HomePageLogo from '../components/molecules/homePageLogo';
-import BottomSection from '../components/organisms/BottomSection';
 import HomeHeadlineSection from '../components/organisms/HomeHeadlineSection';
 
-import footerImage from '../images/home-images/footer-image.png';
-import Button from '../components/atoms/button';
-import {PlaceholderLinks, PlaceholderLink} from '../components/molecules/placeholderLinks';
-
-import homeImageA from '../images/home-images/home-image-a.png';
-import homeImageB from '../images/home-images/home-image-b.png';
-import homeImageC from '../images/home-images/home-image-c.png';
+import { PlaceholderLinks, PlaceholderLink } from '../components/molecules/placeholderLinks';
 
 const IndexPage = ({ data }) => {
   const homepageData = data.allContentfulHomepageData.edges;
